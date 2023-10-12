11e04944b87b12bb8b83f622f052d75823311798
Daniel9991 <35714347+Daniel9991@users.noreply.github.com>
2023-10-12 10:34:13 -0400

    Implement read words from the Iris dataset

--- src/Main.scala
+++ src/Main.scala
@@ -3,13 +3,15 @@ import org.apache.spark._
 object Main {
   def main(args: Array[String]): Unit ={
 
+    val FILE_PATH = "datasets/iris.data"
+
     val conf = new SparkConf().setMaster("local").setAppName("Sparking2")
     val sc = new SparkContext(conf)
 
-    val lines = sc.textFile("src/README.md")
-    val words = lines.flatMap(words => words.split(" "))
+    val lines = sc.textFile(FILE_PATH)
+    val words = lines.flatMap(words => words.split(","))
     val wordCount = words.count()
 
-    println(s"There are ${wordCount} words in the README")
+    println(s"There are ${wordCount} words in the Iris dataset")
   }
 }
