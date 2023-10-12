4fe1965c7a9254635f343a3c76b45aecf7e14bf3
Daniel9991 <35714347+Daniel9991@users.noreply.github.com>
2023-10-12 11:35:03 -0400

    Read from a csv

--- src/Main.scala
+++ src/Main.scala
@@ -1,17 +1,25 @@
+import org.apache.spark
 import org.apache.spark._
+import org.apache.spark.rdd.RDD
 
 object Main {
+
+  val sc = new SparkContext(new SparkConf().setMaster("local").setAppName("Sparking2"))
+
   def main(args: Array[String]): Unit ={
 
     val FILE_PATH = "datasets/iris.data"
 
-    val conf = new SparkConf().setMaster("local").setAppName("Sparking2")
-    val sc = new SparkContext(conf)
+    val data = readCSV(FILE_PATH)
+    val lineSizes = data.map(_.length)
+    val sum = lineSizes.sum()
 
-    val lines = sc.textFile(FILE_PATH)
-    val words = lines.flatMap(words => words.split(","))
-    val wordCount = words.count()
+    println(s"There are ${sum} words in the Iris dataset")
+  }
 
-    println(s"There are ${wordCount} words in the Iris dataset")
+  def readCSV(filePath: String): RDD[Array[String]] ={
+
+    val lines = sc.textFile(filePath)
+    lines.map(line => line.split(",").map(_.trim))
   }
 }
